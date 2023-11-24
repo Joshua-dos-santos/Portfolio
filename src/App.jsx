be0211faaf91baeae4415282a87c93c5f083762b
Joshua-dos-santos <74915903+Joshua-dos-santos@users.noreply.github.com>
2023-11-24 11:59:31 +0100

    Added Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 // App.js
 import React from 'react';
-import Navbar from './components/Navbar'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
 import Home from './components/Home';
+import Projects from './components/Projects'; // Import other components/pages as needed
 
 function App() {
   return (
-    <div className='bg-cyan-900 h-[calc(100vh)]'>
-      <main>
-        <div>
-          <Home/>
-        </div>
-      </main>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/projects' element={<Projects/>}/>
+      </Routes>
+    </Router>
   );
 }
 
